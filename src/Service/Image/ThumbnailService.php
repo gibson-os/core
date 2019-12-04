ed978df78e0e2c554e24c3230fc9ce85dbf1388c
Benjamin Wollenweber <ich@wollis-page.de>
2019-12-04 22:45:59 +0100

    Fix thumbnail service

--- src/Service/Image/ThumbnailService.php
+++ src/Service/Image/ThumbnailService.php
@@ -13,7 +13,7 @@ use GibsonOS\Core\Exception\Image\CreateError;
  */
 class ThumbnailService extends ManipulateService
 {
-    const POSITIONS = [
+    private const POSITIONS = [
         16 => 144,
         32 => 112,
         48 => 64,
@@ -25,22 +25,23 @@ class ThumbnailService extends ManipulateService
     /**
      * @throws CreateError
      */
-    public function create(int $width = 544, int $height = 256): Image
+    public function generate(Image $image, int $width = 544, int $height = 256): Image
     {
         /** @var Image[] $images */
         $images = [];
 
         foreach (self::POSITIONS as $size => $position) {
-            $image = parent::create($size, $size);
+            $imageClone = clone $image;
+            $this->resizeCentered($imageClone, $size, $size);
             $images[$size] = $image;
         }
 
-        $image = parent::create($width, $height);
+        $thumbnail = $this->create($width, $height);
 
         foreach (self::POSITIONS as $size => $position) {
-            $this->copy($images[$size], $image, $position);
+            $this->copy($images[$size], $thumbnail, $position);
         }
 
-        return $image;
+        return $thumbnail;
     }
 }
