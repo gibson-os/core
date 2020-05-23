ebc872a39c4655575db1d4de8e25237b8b1ca664
Benjamin Wollenweber <ich@wollis-page.de>
2020-05-23 20:28:18 +0200

    Refactor index action

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,12 +3,26 @@ declare(strict_types=1);
 
 namespace GibsonOS\Core\Controller;
 
+use GibsonOS\Core\Exception\DateTimeError;
+use GibsonOS\Core\Exception\GetError;
+use GibsonOS\Core\Exception\LoginRequired;
+use GibsonOS\Core\Exception\PermissionDenied;
+use GibsonOS\Core\Exception\Repository\SelectError;
+use GibsonOS\Core\Repository\ModuleRepository;
+use GibsonOS\Core\Repository\SettingRepository;
 use GibsonOS\Core\Service\Response\AjaxResponse;
 
 class IndexController extends AbstractController
 {
-    public function index(): AjaxResponse
+    /**
+     * @throws DateTimeError
+     * @throws GetError
+     * @throws LoginRequired
+     * @throws PermissionDenied
+     * @throws SelectError
+     */
+    public function index(DesktopController $desktopController, ModuleRepository $moduleRepository, SettingRepository $settingRepository): AjaxResponse
     {
-        return $this->returnSuccess();
+        return $desktopController->index($moduleRepository, $settingRepository);
     }
 }
