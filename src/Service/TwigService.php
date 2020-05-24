981698deb38c50851197842e7f5e5725a1d22460
Benjamin Wollenweber <ich@wollis-page.de>
2020-05-24 22:51:21 +0200

    Add module template dirs to loader

--- src/Service/TwigService.php
+++ src/Service/TwigService.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace GibsonOS\Core\Service;
 
+use GibsonOS\Core\Exception\GetError;
 use Twig\Environment;
+use Twig\Error\LoaderError;
 use Twig\Loader\FilesystemLoader;
 
 class TwigService
@@ -13,10 +15,42 @@ class TwigService
      */
     private $twig;
 
-    public function __construct()
+    /**
+     * @throws GetError
+     * @throws LoaderError
+     */
+    public function __construct(DirService $dirService)
     {
+        $projectPath = realpath(
+            dirname(__FILE__) . DIRECTORY_SEPARATOR .
+            '..' . DIRECTORY_SEPARATOR .
+            '..' . DIRECTORY_SEPARATOR
+        );
         $loader = new FilesystemLoader();
-        $loader->addPath(realpath(dirname(__FILE__)) . '/../../template', 'core');
+        $loader->addPath($projectPath . 'template', 'core');
+        $projectPath = realpath(
+            $projectPath .
+            '..' . DIRECTORY_SEPARATOR .
+            '..' . DIRECTORY_SEPARATOR .
+            '..' . DIRECTORY_SEPARATOR
+        );
+
+        foreach ($dirService->getFiles($projectPath . 'vendor' . DIRECTORY_SEPARATOR . 'gibson-os') as $path) {
+            $templatePath = $dirService->addEndSlash($path) . 'templates';
+
+            if (!is_dir($templatePath)) {
+                continue;
+            }
+
+            $loader->addPath(
+                $templatePath,
+                $dirService->removeEndSlash(str_replace(
+                    $projectPath . 'vendor' . DIRECTORY_SEPARATOR . 'gibson-os' . DIRECTORY_SEPARATOR,
+                    '',
+                    $path
+                ))
+            );
+        }
 
         $this->twig = new Environment($loader);
     }
