9b484d6cdb9e3c9c7b40dfab9862e6f57fb8e8c7
Benjamin Wollenweber <ich@wollis-page.de>
2020-05-28 22:40:50 +0200

    Add cronjob controller

---
+++ src/Controller/CronjobController.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace GibsonOS\Core\Controller;
+
+use GibsonOS\Core\Exception\GetError;
+use GibsonOS\Core\Exception\LoginRequired;
+use GibsonOS\Core\Exception\PermissionDenied;
+use GibsonOS\Core\Service\PermissionService;
+use GibsonOS\Core\Service\Response\AjaxResponse;
+use GibsonOS\Core\Store\CronjobStore;
+
+class CronjobController extends AbstractController
+{
+    /**
+     * @throws GetError
+     * @throws LoginRequired
+     * @throws PermissionDenied
+     */
+    public function index(CronjobStore $cronjobStore, int $limit = 0, int $start = 0, array $sort = []): AjaxResponse
+    {
+        $this->checkPermission(PermissionService::READ);
+
+        $cronjobStore->setLimit($limit, $start);
+        $cronjobStore->setSortByExt($sort);
+
+        return $this->returnSuccess($cronjobStore->getList(), $cronjobStore->getCount());
+    }
+}
---
+++ src/Store/CronjobStore.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace GibsonOS\Core\Store;
+
+use GibsonOS\Core\Model\Cronjob;
+
+class CronjobStore extends AbstractDatabaseStore
+{
+    protected function getTableName(): string
+    {
+        return Cronjob::getTableName();
+    }
+
+    protected function getCountField(): string
+    {
+        return '`id`';
+    }
+
+    protected function getOrderMapping(): array
+    {
+        return [
+            'command' => 'command',
+            'user' => 'user',
+            'last_run' => 'last_rund',
+            'active' => 'active',
+        ];
+    }
+
+    public function getList(): array
+    {
+        $this->table->select(
+            false,
+            '`command` ,' .
+            '`arguments`, ' .
+            '`options`, ' .
+            '`user`, ' .
+            '`last_run`, ' .
+            '`active`'
+        );
+
+        return $this->table->connection->fetchAssocList();
+    }
+}
