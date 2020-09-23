04e29e1c941af751252b292a8b86b664c6f353d4
Benjamin Wollenweber <ich@wollis-page.de>
2020-09-23 11:34:03 +0200

    Add abstract test

---
+++ tests/unit/AbstractTest.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace GibsonOS\UnitTest;
+
+use Codeception\Test\Unit;
+use GibsonOS\Core\Service\ServiceManagerService;
+
+class AbstractTest extends Unit
+{
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+
+        $this->serviceManagerService = new ServiceManagerService();
+        putenv('TIMEZONE=Europe/Berlin');
+        putenv('MYSQL_HOST=gos_mysql');
+        putenv('MYSQL_DATABASE=gibson_os_test');
+        putenv('MYSQL_USER=root');
+        putenv('MYSQL_PASS=67yhnkMR');
+    }
+}
