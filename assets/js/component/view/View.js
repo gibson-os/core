c7a59a11d3b62968578f7a44f2b1b53becf29d41
Benjamin Wollenweber <ich@wollis-page.de>
2020-10-31 23:57:51 +0100

    Add view component

---
+++ assets/js/component/view/View.js
@@ -0,0 +1,24 @@
+Ext.define('GibsonOS.core.component.view.View', {
+    extend: 'Ext.view.View',
+    alias: ['widget.gosCoreComponentViewView'],
+    border: false,
+    flex: 1,
+    enablePagingBar: true,
+    enableToolbar: true,
+    enableKeyEvents: true,
+    enableClickEvents: true,
+    enableContextMenu: true,
+    initComponent: function() {
+        let me = this;
+
+        me = GibsonOS.decorator.Panel.init(me);
+
+        if (typeof(me.getColumns) === 'function') {
+            me.columns = me.getColumns();
+        }
+
+        me.callParent();
+
+        GibsonOS.decorator.Panel.addListeners(me);
+    }
+});
