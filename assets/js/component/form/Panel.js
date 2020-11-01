be98762d18d0fd5db793a87afd59a2dd5c06522a
Benjamin Wollenweber <ich@wollis-page.de>
2020-11-01 10:25:57 +0100

    Add form panel

---
+++ assets/js/component/form/Panel.js
@@ -0,0 +1,42 @@
+Ext.define('GibsonOS.core.component.form.Panel', {
+    extend: 'Ext.form.Panel',
+    alias: ['widget.gosCoreComponentFormPanel'],
+    buttonAlign: 'center',
+    frame: true,
+    flex: 1,
+    defaults: {
+        xtype: 'gosCoreComponentPanel'
+    },
+    enableToolbar: true,
+    enableKeyEvents: true,
+    enableClickEvents: false,
+    enableContextMenu: false,
+    initComponent: function() {
+        let me = this;
+
+        me = GibsonOS.decorator.Panel.init(me);
+
+        me.callParent();
+
+        GibsonOS.decorator.Panel.addListeners(me);
+
+        me.getForm().on('actioncomplete', function(form, action, options) {
+            let responseText = Ext.decode(action.response.responseText);
+            GibsonOS.checkResponseForLogin(responseText);
+            GibsonOS.checkResponseForErrorMessage(responseText, action);
+
+            form.getFields().each(function(field) {
+                field.originalValue = field.getValue();
+            });
+        }, this, {
+            priority: 999
+        });
+        me.getForm().on('actionfailed', function(form, action, options) {
+            let responseText = Ext.decode(action.response.responseText);
+            GibsonOS.checkResponseForLogin(responseText);
+            GibsonOS.checkResponseForErrorMessage(responseText, action);
+        }, this, {
+            priority: 999
+        });
+    }
+});
