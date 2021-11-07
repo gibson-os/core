6087f0c9254242295fe95db7bcbaaf279773b6cb
Benjamin Wollenweber <ich@wollis-page.de>
2021-11-07 22:25:32 +0100

    Add module scan command

---
+++ src/Command/Module/ScanCommand.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace GibsonOS\Core\Command\Module;
+
+use GibsonOS\Core\Command\AbstractCommand;
+use GibsonOS\Core\Exception\GetError;
+use GibsonOS\Core\Exception\Model\SaveError;
+use GibsonOS\Core\Service\ModuleService;
+use Psr\Log\LoggerInterface;
+
+class ScanCommand extends AbstractCommand
+{
+    public function __construct(private ModuleService $moduleService, LoggerInterface $logger)
+    {
+        parent::__construct($logger);
+    }
+
+    /**
+     * @throws GetError
+     * @throws SaveError
+     */
+    protected function run(): int
+    {
+        $this->moduleService->scan();
+
+        return 0;
+    }
+}
