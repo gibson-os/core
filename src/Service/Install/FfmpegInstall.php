995da238d90d5bd027674f1e25f62e10c429a71d
Benjamin Wollenweber <ich@wollis-page.de>
2022-01-11 18:34:22 +0100

    Add ffmpeg install

---
+++ src/Service/Install/FfmpegInstall.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace GibsonOS\Core\Service\Install;
+
+use Generator;
+use GibsonOS\Core\Dto\Install\Configuration;
+use GibsonOS\Core\Service\InstallService;
+use GibsonOS\Core\Service\PriorityInterface;
+
+class FfmpegInstall extends AbstractInstall implements PriorityInterface, SingleInstallInterface
+{
+    public function install(string $module): Generator
+    {
+        yield $ffmpegPathInput = $this->getEnvInput('FFMPEG_PATH', 'What is the FFMPEG path?');
+        yield (new Configuration('FFMPEG configuration generated!'))
+            ->setValue('FFMPEG_PATH', $ffmpegPathInput->getValue() ?? '')
+        ;
+    }
+
+    public function getPart(): string
+    {
+        return InstallService::PART_CONFIG;
+    }
+
+    public function getPriority(): int
+    {
+        return 800;
+    }
+}
