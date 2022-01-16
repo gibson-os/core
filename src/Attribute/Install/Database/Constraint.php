41551c64a9b48b1b646ff54619c8036c4627b7dc
Benjamin Wollenweber <ich@wollis-page.de>
2022-01-16 22:56:44 +0100

    Add constraint install

--- src/Attribute/Install/Database/Constraint.php
+++ src/Attribute/Install/Database/Constraint.php
@@ -9,14 +9,20 @@ use GibsonOS\Core\Model\AbstractModel;
 #[Attribute(Attribute::TARGET_PROPERTY | Attribute::IS_REPEATABLE)]
 class Constraint
 {
+    public const RULE_CASCADE = 'CASCADE';
+
+    public const RULE_SET_NULL = 'SET NULL';
+
+    public const RULE_SET_DEFAULT = 'SET DEFAULT';
+
     /**
      * @param class-string<AbstractModel> $parentModelClassName
      */
     public function __construct(
         private string $parentColumn = 'id',
         private ?string $parentModelClassName = null,
-        private ?string $onDelete = null,
-        private ?string $onUpdate = null,
+        private string $onDelete = self::RULE_CASCADE,
+        private string $onUpdate = self::RULE_CASCADE,
         private ?string $name = null,
         private ?string $ownColumn = null,
     ) {
@@ -35,12 +41,12 @@ class Constraint
         return $this->parentColumn;
     }
 
-    public function getOnDelete(): ?string
+    public function getOnDelete(): string
     {
         return $this->onDelete;
     }
 
-    public function getOnUpdate(): ?string
+    public function getOnUpdate(): string
     {
         return $this->onUpdate;
     }
---
+++ src/Service/Install/Database/ConstraintInstall.php
@@ -0,0 +1,138 @@
+<?php
+declare(strict_types=1);
+
+namespace GibsonOS\Core\Service\Install\Database;
+
+use Generator;
+use GibsonOS\Core\Attribute\Install\Database\Constraint;
+use GibsonOS\Core\Attribute\Install\Database\Table;
+use GibsonOS\Core\Dto\Install\Success;
+use GibsonOS\Core\Exception\InstallException;
+use GibsonOS\Core\Model\ModelInterface;
+use GibsonOS\Core\Service\Attribute\TableAttribute;
+use GibsonOS\Core\Service\DirService;
+use GibsonOS\Core\Service\EnvService;
+use GibsonOS\Core\Service\Install\AbstractInstall;
+use GibsonOS\Core\Service\InstallService;
+use GibsonOS\Core\Service\PriorityInterface;
+use GibsonOS\Core\Service\ServiceManagerService;
+use mysqlDatabase;
+use Psr\Log\LoggerInterface;
+use ReflectionAttribute;
+use ReflectionClass;
+
+class ConstraintInstall extends AbstractInstall implements PriorityInterface
+{
+    public function __construct(
+        DirService $dirService,
+        ServiceManagerService $serviceManagerService,
+        EnvService $envService,
+        LoggerInterface $logger,
+        private mysqlDatabase $mysqlDatabase,
+        private TableAttribute $tableAttribute
+    ) {
+        parent::__construct($dirService, $serviceManagerService, $envService, $logger);
+    }
+
+    public function install(string $module): Generator
+    {
+        $path = $this->dirService->addEndSlash($module) . 'src' . DIRECTORY_SEPARATOR . 'Model';
+
+        foreach ($this->getFiles($path) as $file) {
+            $className = $this->serviceManagerService->getNamespaceByPath($file);
+            $reflectionClass = new ReflectionClass($className);
+            $tableAttributes = $reflectionClass->getAttributes(Table::class);
+
+            if (count($tableAttributes) === 0) {
+                continue;
+            }
+
+            /** @var ModelInterface $model */
+            $model = new $className();
+            $tableName = $model->getTableName();
+            $constraints = [];
+
+            foreach ($reflectionClass->getProperties() as $reflectionProperty) {
+                /** @psalm-suppress UndefinedMethod */
+                if ($reflectionProperty->getType()?->isBuiltin() ?? false) {
+                    continue;
+                }
+
+                $constraintAttributes = $reflectionProperty->getAttributes(Constraint::class, ReflectionAttribute::IS_INSTANCEOF);
+
+                if (count($constraintAttributes) === 0) {
+                    continue;
+                }
+
+                /** @var Constraint $constraintAttribute */
+                $constraintAttribute = $constraintAttributes[0]->newInstance();
+                /** @psalm-suppress UndefinedMethod */
+                $parentClassName = $reflectionProperty->getType()?->getName();
+                /** @var ModelInterface $parentModel */
+                $parentModel = new $parentClassName();
+                $parentTableName = $parentModel->getTableName();
+                $constraintName = $constraintAttribute->getName() ?? 'fk' . ucfirst($tableName) . ucfirst($parentTableName);
+                $foreignKey = $constraintAttribute->getOwnColumn()
+                    ?? $this->tableAttribute->transformName($reflectionProperty->getName()) . '_id'
+                ;
+                $parentColumn = $constraintAttribute->getParentColumn();
+                $query =
+                    'SELECT COUNT(`CONSTRAINT_NAME`) ' .
+                    'FROM `information_schema`.`KEY_COLUMN_USAGE` ' .
+                    'WHERE `TABLE_NAME`=? ' .
+                    'AND `COLUMN_NAME`=? ' .
+                    'AND `REFERENCED_TABLE_NAME`=? ' .
+                    'AND `REFERENCED_COLUMN_NAME`=?'
+                ;
+
+                if (!$this->mysqlDatabase->execute($query, [$tableName, $foreignKey, $parentTableName, $parentColumn])) {
+                    throw new InstallException(sprintf(
+                        'Get constraints for table "%s" failed! Error: %s',
+                        $tableName,
+                        $this->mysqlDatabase->error()
+                    ));
+                }
+
+                if ($this->mysqlDatabase->fetchResult() > 0) {
+                    continue;
+                }
+
+                $constraints[] =
+                    'ADD CONSTRAINT `' . $constraintName . '` ' .
+                    'FOREIGN KEY (`' . $foreignKey . '`) ' .
+                    'REFERENCES `' . $parentTableName . '` ' .
+                    '(`' . $parentColumn . '`) ' .
+                    'ON UPDATE ' . $constraintAttribute->getOnUpdate() . ' ' .
+                    'ON DELETE ' . $constraintAttribute->getOnDelete()
+                ;
+            }
+
+            if (count($constraints) === 0) {
+                continue;
+            }
+
+            $query = 'ALTER TABLE `' . $tableName . '` ' . implode(', ', $constraints);
+            $this->logger->debug($query);
+
+            if (!$this->mysqlDatabase->sendQuery($query)) {
+                throw new InstallException(sprintf(
+                    'Add constraints for table "%s" failed! Error: %s',
+                    $tableName,
+                    $this->mysqlDatabase->error()
+                ));
+            }
+
+            yield new Success(sprintf('Constraints for table "%s" installed!', $tableName));
+        }
+    }
+
+    public function getPart(): string
+    {
+        return InstallService::PART_DATABASE;
+    }
+
+    public function getPriority(): int
+    {
+        return 699;
+    }
+}
