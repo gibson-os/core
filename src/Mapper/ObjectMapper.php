986b7d7600928b21c64e1b42abde06e9fad5b31a
Benjamin Wollenweber <ich@wollis-page.de>
2022-02-11 22:34:13 +0100

    Change isset to array key exists

--- src/Mapper/ObjectMapper.php
+++ src/Mapper/ObjectMapper.php
@@ -28,7 +28,7 @@ class ObjectMapper
 
         if ($reflectionConstructor !== null) {
             foreach ($reflectionConstructor->getParameters() as $reflectionParameter) {
-                if (!isset($properties[$reflectionParameter->getName()])) {
+                if (array_key_exists($reflectionParameter->getName(), $properties)) {
                     if (!$reflectionParameter->isDefaultValueAvailable()) {
                         throw new MapperException(sprintf(
                             'Value for constructor parameter "%s" of class "%s" is missing!',
