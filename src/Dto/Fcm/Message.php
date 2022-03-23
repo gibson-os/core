8d231637cc30c30a5e1449a5dbe06f8c40e96624
Benjamin Wollenweber <ich@wollis-page.de>
2022-03-23 15:35:38 +0100

    Add more infos to fcm message

--- src/Dto/Fcm/Message.php
+++ src/Dto/Fcm/Message.php
@@ -8,12 +8,20 @@ use JsonSerializable;
 
 class Message implements JsonSerializable
 {
+    public const OPTION_VIBRATION = 1;
+
+    public const OPTION_SOUND = 2;
+
     public function __construct(
         private string $fcmToken,
         private Type $type = Type::NOTIFICATION,
         private ?string $title = null,
         private ?string $body = null,
-        private array $data = []
+        private string $module = 'core',
+        private string $task = 'index',
+        private string $action = 'index',
+        private array $data = [],
+        private int $options = self::OPTION_VIBRATION + self::OPTION_SOUND
     ) {
     }
 
@@ -28,13 +36,17 @@ class Message implements JsonSerializable
             ];
         }
 
-        $data['data'] = [];
+        $data['data'] = [
+            'type' => $this->type->value,
+            'module' => $this->module,
+            'task' => $this->task,
+            'action' => $this->action,
+            'options' => $this->options,
+        ];
 
         if (count($this->data)) {
-            $data['data'] = $data;
+            $data['data']['payload'] = $data;
         }
-
-        $data['data']['type'] = $this->type->value;
 
         return $data;
     }
