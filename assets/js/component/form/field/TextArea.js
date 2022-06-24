cd5c8978228b2a867ddc0818d7703006b47ae98b
Benjamin Wollenweber <ich@wollis-page.de>
2022-06-24 23:26:22 +0200

    Add ext text area

---
+++ assets/js/component/form/field/TextArea.js
@@ -0,0 +1,7 @@
+Ext.define('GibsonOS.module.core.component.form.field.TextArea', {
+    extend: 'Ext.form.TextArea',
+    alias: ['widget.gosCoreComponentFormFieldTextArea'],
+    fieldLabel: 'Text Area',
+    anchor: '100%',
+    border: false,
+});
