44c9924370f14d0aa0d3c99ed8399d95c246783e
Benjamin Wollenweber <ich@wollis-page.de>
2022-07-20 20:59:10 +0200

    Add table service test

---
+++ tests/unit/Service/Command/TableServiceTest.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace GibsonOS\UnitTest\Service\Command;
+
+use GibsonOS\Core\Service\Command\TableService;
+use GibsonOS\UnitTest\AbstractTest;
+
+class TableServiceTest extends AbstractTest
+{
+    private TableService $tableService;
+
+    protected function _before()
+    {
+        $this->tableService = $this->serviceManager->get(TableService::class);
+    }
+
+    /**
+     * @dataProvider getData
+     */
+    public function testGetTable(array $headers, array $content, string $result): void
+    {
+        $this->assertEquals($result, $this->tableService->getTable($headers, $content));
+    }
+
+    public function getData(): array
+    {
+        return [
+            [
+                ['First', 'Last'],
+                [
+                    ['Ford', 'Prefect'],
+                    ['Arthur', 'Dent'],
+                ],
+                'First  | Last   ' . PHP_EOL .
+                '----------------' . PHP_EOL .
+                'Ford   | Prefect' . PHP_EOL .
+                'Arthur | Dent   ' . PHP_EOL,
+            ], [
+                ['Firstname', 'Lastname'],
+                [
+                    ['Ford', 'Prefect'],
+                    ['Arthur', 'Dent'],
+                ],
+                'Firstname | Lastname' . PHP_EOL .
+                '--------------------' . PHP_EOL .
+                'Ford      | Prefect ' . PHP_EOL .
+                'Arthur    | Dent    ' . PHP_EOL,
+            ],
+        ];
+    }
+}
