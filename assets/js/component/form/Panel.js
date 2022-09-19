bf37aef09f9fbee78612b50ddd58cefcfc757b41
Benjamin Wollenweber <ich@wollis-page.de>
2022-09-19 16:37:25 +0200

    Add load form logic to panel

--- assets/js/component/form/Panel.js
+++ assets/js/component/form/Panel.js
@@ -11,7 +11,9 @@ Ext.define('GibsonOS.module.core.component.form.Panel', {
     enableKeyEvents: false,
     enableClickEvents: false,
     enableContextMenu: false,
-    initComponent: function() {
+    url: null,
+    params: {},
+    initComponent() {
         let me = this;
 
         me = GibsonOS.decorator.Panel.init(me);
@@ -20,7 +22,8 @@ Ext.define('GibsonOS.module.core.component.form.Panel', {
 
         GibsonOS.decorator.Panel.addListeners(me);
 
-        me.getForm().on('actioncomplete', function(form, action) {
+        const basicForm = me.getForm();
+        basicForm.on('actioncomplete', function(form, action) {
             let responseText = Ext.decode(action.response.responseText);
             GibsonOS.checkResponseForLogin(responseText);
             GibsonOS.checkResponseForErrorMessage(responseText, action);
@@ -31,12 +34,88 @@ Ext.define('GibsonOS.module.core.component.form.Panel', {
         }, this, {
             priority: 999
         });
-        me.getForm().on('actionfailed', function(form, action) {
+        basicForm.on('actionfailed', function(form, action) {
             let responseText = Ext.decode(action.response.responseText);
             GibsonOS.checkResponseForLogin(responseText);
             GibsonOS.checkResponseForErrorMessage(responseText, action);
         }, this, {
             priority: 999
         });
+
+        if (me.url !== null) {
+            me.on('render', () => {
+                me.loadForm(me.url, me.params);
+            });
+        }
+    },
+    loadForm(url, params) {
+        const me = this;
+
+        me.setLoading(true);
+
+        GibsonOS.Ajax.request({
+            url: url,
+            params: params,
+            success(response) {
+                me.removeAll();
+                me.addFields(Ext.decode(response.responseText).data);
+                me.setLoading(false);
+            }
+        });
+    },
+    addField(name, parameter) {
+        const me = this;
+
+        me.fireEvent('beforeAddField', name, parameter);
+
+        let item = {
+            xtype: parameter.xtype,
+            name: name,
+            value: parameter.value ?? null,
+            checked: parameter.value === true,
+            parameterObject: parameter,
+            fieldLabel: parameter.title
+        };
+
+        if (parameter.config.inputType) {
+            item.inputType = parameter.config.inputType;
+        }
+
+        if (parameter.config.options) {
+            item.store = {
+                fields: ['id', 'name'],
+                data: []
+            }
+
+            Ext.iterate(parameter.config.options, (value, id) => {
+                item.store.data.push({
+                    id: id,
+                    name: value
+                });
+            });
+        }
+
+        if (parameter.image) {
+            me.add({
+                xtype: 'gosCoreComponentFormFieldDisplay',
+                value: '<img src="' + parameter.image + '" alt="image" />',
+                fieldLabel: '&nbsp;',
+                labelSeparator: ''
+            });
+        }
+
+        me.add(item);
+        me.fireEvent('afterAddField', name, parameter);
+    },
+    addFields(parameters) {
+        const me = this;
+
+        me.fireEvent('beforeAddFields', parameters);
+
+        Ext.iterate(parameters, (name, parameter) => {
+            me.addField(name, parameter)
+        });
+
+        me.fireEvent('afterAddFields', parameters);
     }
 });
