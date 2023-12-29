bfdbdca8281be3eb730df994533e9abaa7e74a95
Benjamin Wollenweber <ich@wollis-page.de>
2023-12-29 11:37:01 +0100

    Add process tracing

--- src/Service/ProcessService.php
+++ src/Service/ProcessService.php
@@ -8,8 +8,10 @@ use Psr\Log\LoggerInterface;
 
 class ProcessService
 {
-    public function __construct(private readonly LoggerInterface $logger)
-    {
+    public function __construct(
+        private readonly LoggerInterface $logger,
+        private readonly TracerService $tracerService,
+    ) {
     }
 
     /**
@@ -41,26 +43,32 @@ class ProcessService
      */
     public function execute(string $command): string
     {
+        $this->tracerService->startSpan('Execute process', ['command' => $command]);
         $this->logger->debug(sprintf('Execute process "%s"', $command));
         $return = exec($command);
+        $this->tracerService->stopSpan();
 
         return $return !== false ? $return : throw new ProcessError('Exec error!');
     }
 
     public function executeAsync(string $command): void
     {
+        $this->tracerService->startSpan('Execute async process', ['command' => $command]);
         $this->logger->debug(sprintf('Execute async process "%s"', $command));
 
         system($command . '> /dev/null 2>/dev/null &');
+        $this->tracerService->stopSpan();
     }
 
     public function kill(int $pid): bool
     {
+        $this->tracerService->startSpan('Kill process', ['pid' => $pid]);
         $this->logger->debug(sprintf('Kill process %d', $pid));
 
         $return = 0;
         $out = [];
         exec('kill -s 9 ' . $pid, $out, $return);
+        $this->tracerService->stopSpan();
 
         return $return === 0;
     }
