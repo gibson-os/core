c32ebd2bc1beae22819ca49a17552ead48a1e18e
Benjamin Wollenweber <ich@wollis-page.de>
2024-09-24 22:54:01 +0200

    Fix annotation

--- src/Model/Event/Element.php
+++ src/Model/Event/Element.php
@@ -73,7 +73,7 @@ class Element extends AbstractModel implements Serializable, JsonSerializable
     protected ?Element $parent = null;
 
     /**
-     * @var Element[]|null
+     * @var Element[]
      */
     #[Constraint('parent', Element::class)]
     protected array $children = [];
