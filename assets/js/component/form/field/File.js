071db1b3d0989571222bc984cc9c9066ee0034ae
Benjamin Wollenweber <ich@wollis-page.de>
2024-10-01 13:59:51 +0200

    Fix file field

--- assets/js/component/form/field/File.js
+++ assets/js/component/form/field/File.js
@@ -1,6 +1,6 @@
 Ext.define('GibsonOS.module.core.component.form.field.FileField', {
     extend: 'Ext.form.field.File',
-    alias: ['widget.gosCoreComponentFormFieldFileField'],
+    alias: ['widget.gosCoreComponentFormFieldFile'],
     fieldLabel: 'File Field',
     anchor: '100%',
     border: false,
