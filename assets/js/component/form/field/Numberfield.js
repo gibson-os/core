723727f75c00eb88084b193dfd7ea34062d856da
Benjamin Wollenweber <ich@wollis-page.de>
2024-11-18 17:37:47 +0100

    Add number field multiplier listener

--- assets/js/component/form/field/Numberfield.js
+++ assets/js/component/form/field/Numberfield.js
@@ -5,4 +5,30 @@ Ext.define('GibsonOS.module.core.component.form.field.NumberField', {
     minValue: 0,
     anchor: '100%',
     border: false,
+    initComponent() {
+        const me = this;
+        let config = me.parameterObject.config;
+
+        me.callParent();
+
+        me.on('afterrender', () => {
+            if (!config.listeners) {
+                return;
+            }
+
+            Ext.iterate(config.listeners, function(fieldName, options) {
+                const listenerField =  me.up('form').getForm().findField(fieldName);
+
+                if (!listenerField) {
+                    return true;
+                }
+
+                listenerField.on('change', (field, value) => {
+                    if (options.multiplier) {
+                        me.setValue(value * options.multiplier);
+                    }
+                });
+            });
+        });
+    }
 });
